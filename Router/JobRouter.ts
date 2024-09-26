0f7b43548dcd67d316fdf81c95aa7c9fe7128229
pradip jedhe <pradipjedhe69@gmail.com>
2024-09-26 22:41:46 +0530

    fix the forget password error

--- Router/JobRouter.ts
+++ Router/JobRouter.ts
@@ -8,6 +8,6 @@ router.post("/Admin/PostJobs/:id", jwtAuthMiddleware, PostJobCompany);
 router.put("/Admin/Jobs/Update/:id", jwtAuthMiddleware, UpdateJobs)
 // router.get("/GetJobs/ById/:id", jwtAuthMiddleware, GetAllJobsById)
 router.get("/GetAll/Jobs", GetAllJobs);
-router.get("/GetAll/Jobs/Admin", GetallJobinAdminCreated)
+router.get("/GetAll/Jobs/Admin",jwtAuthMiddleware, GetallJobinAdminCreated)
 
 export default router;
